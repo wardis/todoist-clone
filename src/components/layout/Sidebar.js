a1f1ded5af845b041fe2be04dffca68f636e6608
wardis <wardi2976@live.fr>
2019-11-17 07:26:13 +0000

    sidebar generic layout

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -1,3 +1,40 @@
-import React from 'react'
+import React from "react";
+import {
+  FaChevronDown,
+  FaInbox,
+  FaRegCalendar,
+  FaRegCalendarAlt
+} from "react-icons/fa";
 
-export const Sidebar = () => <p>The Side bar</p>;
+export const Sidebar = () => (
+  <div className="sidebar" data-testid="sidebar">
+    <ul className="sidebar__generic">
+      <li>
+        <span>
+          <FaInbox />
+        </span>
+        <span>Inbox</span>
+      </li>
+      <li>
+        <span>
+          <FaRegCalendar />
+        </span>
+        <span>Today</span>
+      </li>
+      <li>
+        <span>
+          <FaRegCalendarAlt />
+        </span>
+        <span>Next 7 days</span>
+      </li>
+    </ul>
+    <div className="sidebar__middle">
+      <span>
+        <FaChevronDown />
+      </span>
+      <h2>Projects</h2>
+    </div>
+    <ul className="sidebar__projects">Projects go here</ul>
+    Add projects here!!
+  </div>
+);
