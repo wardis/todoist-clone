93e388e072daf816c235df393d68e6a3bf1e8336
wardis <wardi2976@live.fr>
2019-11-17 13:57:19 +0000

    add classnames to sidebar

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -1,40 +1,41 @@
-import React from "react";
+import React from 'react';
 import {
   FaChevronDown,
   FaInbox,
   FaRegCalendar,
   FaRegCalendarAlt
-} from "react-icons/fa";
+} from 'react-icons/fa';
 
 export const Sidebar = () => (
-  <div className="sidebar" data-testid="sidebar">
-    <ul className="sidebar__generic">
-      <li>
+  <div className='sidebar' data-testid='sidebar'>
+    <ul className='sidebar__generic'>
+      <li className='inbox' data-testid='inbox'>
+        >
         <span>
           <FaInbox />
         </span>
         <span>Inbox</span>
       </li>
-      <li>
+      <li className='today' data-testid='today'>
         <span>
           <FaRegCalendar />
         </span>
         <span>Today</span>
       </li>
-      <li>
+      <li className='next_7' data-testid='next_7'>
         <span>
           <FaRegCalendarAlt />
         </span>
         <span>Next 7 days</span>
       </li>
     </ul>
-    <div className="sidebar__middle">
+    <div className='sidebar__middle'>
       <span>
         <FaChevronDown />
       </span>
       <h2>Projects</h2>
     </div>
-    <ul className="sidebar__projects">Projects go here</ul>
+    <ul className='sidebar__projects'>Projects go here</ul>
     Add projects here!!
   </div>
 );
