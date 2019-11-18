18d26ff98ba7b1cebd12b9a332523de536bf9616
wardis <wardi2976@live.fr>
2019-11-18 18:43:44 +0000

    fetch tasks from database

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Checkbox } from './Checkbox';
-import { useTasks  } from '../hooks'; 
+import { useTasks } from '../hooks';
+import { collatedTasks } from '../constants';
+import { getTitle, getCollatedTitle, collatedTasksExist } from '../helpers';
+import { useSelectedProjectValue, useProjectsValue } from '../context';
 
 export const Tasks = () => {
-  const {tasks} = useTasks('1');
-  const projectName = '';
+  const { selectedProject } = useSelectedProjectValue();
+  const { projects } = useProjectsValue();
+  const { tasks } = useTasks(selectedProject);
+
+  let projectName = '';
+
+  if (projects && selectedProject && !collatedTasksExist(selectedProject)) {
+    projectName = getTitle(projects, selectedProject).name;
+    console.log('pn 1:', projectName);
+  }
+
+  if (collatedTasksExist(selectedProject) && selectedProject) {
+    projectName = getCollatedTitle(collatedTasks, selectedProject).name;
+    console.log('pn 2:', projectName);
+  }
+
+  console.log('tasks: ', tasks);
+
+  useEffect(() => {
+    document.title = `${projectName}: Todoist`;
+  });
 
   return (
     <div className='tasks' data-testid='tasks'>
