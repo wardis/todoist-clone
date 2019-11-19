bd1240959cc1813518f26536b4887cef4b6a990f
wardis <wardi2976@live.fr>
2019-11-19 19:42:52 +0000

    add task jsx layout

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { FaRegListAlt, FaRagCalendarAlt } from 'react-icons/fa';
+import { FaRegListAlt, FaRegCalendarAlt } from 'react-icons/fa';
 import { moment } from 'moment';
 import { firebase } from '../firebase';
 import { useSelectedProjectValue } from '../context';
@@ -66,6 +66,72 @@ export const AddTask = ({
         >
           <span className='add-task__plus'>+</span>
           <span className='add-task__text'>Add Task</span>
+        </div>
+      )}
+
+      {(showMain || showQuickAddTask) && (
+        <div className='add-task-main' data-testid='add-task-main'>
+          {showQuickAddTask && (
+            <>
+              <div data-testid='quick-add-task'>
+                <h2 className='header'>Quick Add Task</h2>
+                <span
+                  className='add-task__cancel-x'
+                  data-testid='add-task-quick-cancel'
+                  onClick={() => {
+                    setShowMain(false);
+                    setShowProjectOverlay(false);
+                    setShowQuickAddTask(false);
+                  }}
+                >
+                  X
+                </span>
+              </div>
+            </>
+          )}
+          <p>Project overlay here</p>
+          <p>TaskDate here</p>
+          <input
+            className='add-task__content'
+            data-testid='add-task-content'
+            type='text'
+            value={task}
+            onChange={e => setTask(e.target.value)}
+          />
+          <button
+            type='button'
+            className='add-task__submit'
+            data-testid='add-task'
+            onClick={() => addTask()}
+          >
+            Add Task
+          </button>
+          {!showQuickAddTask && (
+            <span
+              className='add-task__cancel'
+              data-testid='add-task-main-cancel'
+              onClick={() => {
+                setShowMain(false);
+                setShowProjectOverlay(false);
+              }}
+            >
+              Cancel
+            </span>
+          )}
+          <span
+            className="add-task__project"
+            data-testid="show-project-overlay"
+            onClick={()=>setShowProjectOverlay(!showProjectOverlay)}
+          >
+            <FaRegListAlt />
+          </span>
+          <span
+            className="add-task__date"
+            data-testid="show-task-date-overlay"
+            onClick={()=>setShowTaskDate(!showTaskDate)}
+          >
+            <FaRegCalendarAlt />
+          </span>
         </div>
       )}
     </div>
